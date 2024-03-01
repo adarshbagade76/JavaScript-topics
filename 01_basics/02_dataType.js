76a2fe09d9f733e10fe18ee4f769488eb4c98036
Adarsh <adarshbagade76@gmail.com>
2024-03-01 12:25:50 +0530

    data type and conversion file

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,26 @@
+// Data Types
+
+let length=16 //number 
+let name="Sam"//string
+let isLoginUser=true//Boolean
+let data=null//null
+let id;//undefined
+let x=BigInt("122121212151215022510220012102102512102200")
+       //symbol
+console.log(typeof length)//number
+console.log(typeof name)//string
+console.log(typeof isLoginUser)//boolean
+console.log(typeof data) //obj
+console.log(typeof id)//undefined
+console.log(typeof x)//bigint
+
+
+// non-primitive Data type
+const cars=["Audi","Tata","Volvo"]// object
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};// object
+//Data
+console.log(typeof cars)//object
+console.log(typeof person)//object
+
+
+
